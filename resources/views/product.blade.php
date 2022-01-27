9389c151db0b14db7c111e5717ced470bd060dad
netwarp <resethread@gmail.com>
2022-01-27 16:38:26 +0100

    center text

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card">
+    <div class="card text-center">
         <div class="card-image">
             <img src="/image/{{ $product->image }}" alt="{{ $product->title }}">
         </div>
         <div class="card-content">
-            <h1>{{ $product->title }}</h1>
+            <h1>{{ $product->title ?? '' }}</h1>
             <div>
-                {{ $product->description }}
+                {{ $product->description ?? '' }}
+            </div>
+
+            <div>
+                {{ $product->price ?? '' }}
             </div>
 
             <div class="button-store">
